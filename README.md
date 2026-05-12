cc521b907607b7d13b6ae9241d2e44e1971b086d
Rachel Carboni <rachelcarboni@gmail.com>
2026-05-12 15:41:35 +0200

    edited collab_trial.R script to capitilize message and updated README

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Hello again 123 test
 hello 
 hello
 trying branch
+
+
+Rachel edited collab_trial.R file on 12.05.2026 as part of an excercise on learning how to make branches and collaborate.
--- collab_trial.R
+++ collab_trial.R
@@ -4,3 +4,10 @@ message <- "Hello, this is a message. can you capitalize the entire message!"
 
 # 3. Print Results to Console
 print(message)
+
+# EDIT
+
+# use touppper to capitilize a message
+message_cap <- toupper("Hello, this is a message. can you capitalize the entire message!")
+
+print(message_cap)
